76b966551f54100e7c50ac4c36c2647e3f1410d5
Xingtitan <thanhdat021293@gmail.com>
2022-04-23 16:39:40 +0700

    lab2.5

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const http = require("http");
-
 const express = require("express");
-
 const app = express();
+const bodyParser = require("body-parser");
 
-app.use('/add-product', (req, res, next) => {
-  console.log("In the middleware");
-  res.send('<h1>The "Add Product" Page</h1>');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input name="title" type="text"><button type="submit">Submit</button></form>'
+  );
 });
 
-app.use('/', (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from Express!</h1>");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-const server = http.createServer(app);
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello World</h1>");
+});
 
-server.listen(3000);
+app.listen(3000);
