a36de6e7631c0a480499ccbbcb03279acbec64d4
Xingtitan <thanhdat021293@gmail.com>
2022-04-24 00:31:43 +0700

    lab2.9

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+// GET /admin/add-product
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input name="title" type="text"><button type="submit">Add Product</button></form>'
+    '<form action="/admin/add-product" method="POST"><input name="title" type="text"><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+// POST /admin/add-product
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
