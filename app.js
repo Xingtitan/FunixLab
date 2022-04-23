3280b2cae5723b34efb62428fab744d19de1d3b5
Xingtitan <thanhdat021293@gmail.com>
2022-04-23 23:12:14 +0700

    lab2.8

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(shopRoutes);
 app.use(adminRoutes);
 
+app.use((req, res) => {
+  res.status(404).send('<h1>Page Not Found</h1>')
+})
+
 app.listen(3000);
