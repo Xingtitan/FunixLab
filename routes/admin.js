184c65d65fdb761c47cde47acabcd3c1a260b767
Xingtitan <thanhdat021293@gmail.com>
2022-05-04 16:56:46 +0700

    lab3.4

--- routes/admin.js
+++ routes/admin.js
@@ -8,14 +8,14 @@ const router = express.Router();
 
 const products = [];
 
-// GET /admin/add-product
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { pageTitle: "Add Product" });
 });
 
-// POST /admin/add-product
+// /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  products.push({title: req.body.title});
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
